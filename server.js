f17dd93889ce8dd96bb8d85b3824cb91f8034844
Fatkullin <fatkullinrustam@gmail.com>
2013-11-03 16:46:41 +0400

    s

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-var http = require('http')
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(80);
+function MessageProcess( a_conn, a_msg )
+{
+	console.log( "[MSG]: " + a_msg );
+}
+
+function Start()
+{
+	WEB_SOCKET_SERVER_PORT 	= 1024;	
+	g_webSocketServerCretor = require( 'ws' ).Server;
+	g_server 				= new g_webSocketServerCretor( { port : WEB_SOCKET_SERVER_PORT } );
+	
+	g_server.on( 'connection', function( a_ws )
+	{
+		console.log( 'Client connected.' );
+		a_ws.on( 'message', MessageProcess );
+	} );
+
+	console.log( 'Server started.' );
+}
+
+Start();
