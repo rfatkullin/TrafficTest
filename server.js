e2d4f80e6074ae430b4e69cc39405e789ce80ec6
Fatkullin Rustam <fatkullinrustam@gmail.com>
2013-11-03 15:22:56 +0400

    Fix bug

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's work!");
-  response.end();
-}).listen(80);
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
